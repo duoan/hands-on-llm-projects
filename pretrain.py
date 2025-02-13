387d5d9b0f7326b926c764ad643a932c0bcde564
Victor An <2378740+duoan@users.noreply.github.com>
2025-02-12 21:09:41 -0800

    fix output path

--- pretrain.py
+++ pretrain.py
@@ -27,7 +27,7 @@ from transformers import (
 
 dataset_path = "roneneldan/TinyStories"
 model_path = "roneneldan/TinyStories-1M"
-output_path = f"results/${model_path}/pt"
+output_path = f"results/{model_path}/pt"
 tokenized_datapath = os.path.join("data", dataset_path, "tokenized_dataset")
 
 os.makedirs(output_path, exist_ok=True)
